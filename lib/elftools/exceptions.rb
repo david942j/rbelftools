670e1805e23a4d875695cb6ffbed042284725a15
William Woodruff <william@yossarian.net>
2020-02-03 09:10:09 +0100

    elftools: Add finer-grained exceptions (#43)
    * Refactors ELFError into distinct child errors for specific
    failures
    * rubocop: cop fixes

--- lib/elftools/exceptions.rb
+++ lib/elftools/exceptions.rb
@@ -3,4 +3,13 @@
 module ELFTools
   # Being raised when parsing error.
   class ELFError < StandardError; end
+
+  # Raised on invalid ELF magic.
+  class ELFMagicError < ELFError; end
+
+  # Raised on invalid ELF class (EI_CLASS).
+  class ELFClassError < ELFError; end
+
+  # Raised on invalid ELF data encoding (EI_DATA).
+  class ELFDataError < ELFError; end
 end
