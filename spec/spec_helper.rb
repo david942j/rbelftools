cb7a5e2f40521f68a82a997a8d0c77c9fda5c430
david942j <david942j@gmail.com>
2022-03-27 22:35:15 +0800

    Don't specify SimpleCov formatters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  [SimpleCov::Formatter::HTMLFormatter]
-)
+
 SimpleCov.start do
   add_filter '/spec/'
 end
