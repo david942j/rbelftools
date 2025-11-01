a680ae020b60e3d7611919a5f5b14169006dce6b
david942j <david942j@gmail.com>
2025-11-01 03:16:04 +0000

    SimpleCov JSON formatter
    Signed-off-by: david942j <david942j@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov_json_formatter'
 
 SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+                                                       SimpleCov::Formatter::JSONFormatter,
+                                                       SimpleCov::Formatter::HTMLFormatter
+                                                     ])
   add_filter '/spec/'
 end
