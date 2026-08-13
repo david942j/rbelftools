e76dc9f0a6c8725644f21b589cda91342ea849fc
david942j <david942j@gmail.com>
2026-08-13 00:05:02 +0000

    Move Relocation and Symbol into their own files

---
+++ lib/elftools/relocation.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module ELFTools
+  # A relocation entry.
+  #
+  # Can be either a REL or RELA relocation.
+  class Relocation
+    attr_reader :header # @return [ELFTools::Structs::ELF_Rel, ELFTools::Structs::ELF_Rela] Rel(a) header.
+    attr_reader :stream # @return [#pos=, #read] Streaming object.
+
+    # Instantiate a {Relocation} object.
+    def initialize(header, stream)
+      @header = header
+      @stream = stream
+    end
+
+    # +r_info+ contains sym and type, use two methods
+    # to access them easier.
+    # @return [Integer] sym infor.
+    def r_info_sym
+      header.r_info >> mask_bit
+    end
+    alias symbol_index r_info_sym
+
+    # +r_info+ contains sym and type, use two methods
+    # to access them easier.
+    # @return [Integer] type infor.
+    def r_info_type
+      header.r_info & ((1 << mask_bit) - 1)
+    end
+    alias type r_info_type
+
+    private
+
+    def mask_bit
+      header.elf_class == 32 ? 8 : 32
+    end
+  end
+end
--- lib/elftools/sections/relocation_section.rb
+++ lib/elftools/sections/relocation_section.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'elftools/constants'
+require 'elftools/relocation'
 require 'elftools/sections/section'
 require 'elftools/structs'
 
@@ -67,43 +68,6 @@ module ELFTools
         rel.read(stream)
         Relocation.new(rel, stream)
       end
-    end
-  end
-
-  # A relocation entry.
-  #
-  # Can be either a REL or RELA relocation.
-  # XXX: move this to an independent file?
-  class Relocation
-    attr_reader :header # @return [ELFTools::Structs::ELF_Rel, ELFTools::Structs::ELF_Rela] Rel(a) header.
-    attr_reader :stream # @return [#pos=, #read] Streaming object.
-
-    # Instantiate a {Relocation} object.
-    def initialize(header, stream)
-      @header = header
-      @stream = stream
-    end
-
-    # +r_info+ contains sym and type, use two methods
-    # to access them easier.
-    # @return [Integer] sym infor.
-    def r_info_sym
-      header.r_info >> mask_bit
-    end
-    alias symbol_index r_info_sym
-
-    # +r_info+ contains sym and type, use two methods
-    # to access them easier.
-    # @return [Integer] type infor.
-    def r_info_type
-      header.r_info & ((1 << mask_bit) - 1)
-    end
-    alias type r_info_type
-
-    private
-
-    def mask_bit
-      header.elf_class == 32 ? 8 : 32
     end
   end
 end
